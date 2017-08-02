90b441a6f25ac33d97f92d65731a9a53f00d77a4
niuzhimeng <295290968@qq.com>
2017-08-02 18:22:20 +0800

    读取excel + 输出

--- src/main/java/pio/PoiTest.java
+++ src/main/java/pio/PoiTest.java
@@ -1,14 +1,19 @@
 package pio;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
 import pio.utils.OkHttpUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +22,17 @@ import java.util.List;
  * Created by Nzm on 2017/8/2.
  */
 public abstract class PoiTest {
-
+    /**
+     * 读取excel
+     *
+     * @param file 文件对象
+     * @throws Exception
+     */
     public void read(File file) throws Exception {
-
+        //返回结果集合
+        List<String> resultList = new ArrayList<>();
         //一行元素的集合
-        List<String> cellList = new ArrayList<>();
+        List<String> rowList = new ArrayList<>();
         //当前单元格的元素
         String currentCell;
 
@@ -43,16 +54,60 @@ public abstract class PoiTest {
                 } else {
                     currentCell = NumberToTextConverter.toText(cells.getCell(j).getNumericCellValue());
                 }
-                cellList.add(currentCell);
+                rowList.add(currentCell);
             }
-            String url = appendUrl(cellList);
-            cellList.clear();
-            //OkHttpUtils.get(url);
-            System.out.println(url);
+            String url = appendUrl(rowList);
+            //发送http请求
+//            String resultString = OkHttpUtils.get(url);
+            String resultString = "返回数据";
+            StringBuilder parameterString = new StringBuilder();
+            for (String parameter : rowList) {
+                parameterString.append(parameter).append("; ");
+            }
 
+            resultList.add(parameterString.toString() + "," + resultString);
+            Thread.sleep(30);
+            System.out.println(url);
+            rowList.clear();
         }
+        //调用输出方法
+        write(resultList);
 
     }
+
+    /**
+     * 导出excel
+     *
+     * @param contentList 返回结果的list<入参,返回json>
+     * @throws Exception
+     */
+    public void write(List<String> contentList) throws Exception {
+
+        // 创建新的Excel 工作簿
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        // 在Excel工作簿中建一工作表，其名为缺省值
+        XSSFSheet sheet = workbook.createSheet();
+
+        int row_num = contentList.size();
+        for (int i = 0; i < row_num; i++) {
+            XSSFRow row = sheet.createRow(i);
+            String[] ss = contentList.get(i).split(",");
+            for (int j = 0; j < 2; j++) {
+                XSSFCell cell = row.createCell(j);
+                cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                cell.setCellValue(ss[j]);
+            }
+        }
+        // 新建一输出文件流
+        FileOutputStream fOut = new FileOutputStream(new File("D:\\write.xlsx"));
+        // 把相应的Excel 工作簿存盘
+        workbook.write(fOut);
+        fOut.flush();
+        // 操作结束，关闭文件
+        fOut.close();
+
+    }
+
 
     /**
      * 钩子方法
@@ -68,7 +123,7 @@ public abstract class PoiTest {
      * @param account 账号
      * @param token   token
      */
-    public String appendAccountInfo(String url, String account, String token) {
+    protected String appendAccountInfo(String url, String account, String token) {
         return url += "?account=" + account + "&accessToken=" + token;
     }
 }
--- src/main/java/pio/test/Test.java
+++ src/main/java/pio/test/Test.java
@@ -1,9 +1,17 @@
 package pio.test;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import pio.impl.PoliceIdentity;
 import pio.impl.Traffic;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nzm on 2017/8/2.
@@ -19,12 +27,13 @@ public class Test {
         police.read(file);
     }
 
-
     @org.junit.Test
     public void test2() throws Exception {
         File file = new File("D:\\traffic.xlsx");
         Traffic traffic = new Traffic();
-       // traffic.appendAccountInfo("nzm2","456");
+        traffic.appendAccountInfo("nzm2", "456");
         traffic.read(file);
     }
+
+
 }
